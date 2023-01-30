27fb234497a41df21d2e95580792804e2ac5c5f8
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2023-01-30 13:56:01 +0530

    [Jyoti Rao] Find Fibonacci Series

---
+++ LogicalPrograms/FibonacciSeries.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public class FibonacciSeries
+    {
+        public static int a = 0, b = 0, c = 1, number;
+        //1.Find Fibonacci Series
+        public static void FindFibonacciSeries()
+        {
+            Console.WriteLine("Please enter number as far you want find Fibonacci Series");
+            number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Fibonacci Series:--\n-----------------------");
+            for (int i = 1; i <= number; i++)
+            {
+                a = b;
+                b = c;
+                c = a + b;
+                Console.Write(a + "  ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LogicalPrograms/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+
+namespace LogicalPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Logical Programs\n------------------------------------");
+            Console.WriteLine("Please select any option");
+            Console.WriteLine("1.FindFibonacciSeries\n13.Exit\n");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Console.Clear();
+                    FibonacciSeries.FindFibonacciSeries();
+                    break;
+                case 13:
+                    Console.Clear();
+                    break;
+                default:
+                    Console.Clear();
+                    Console.WriteLine("Please select only given option");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
