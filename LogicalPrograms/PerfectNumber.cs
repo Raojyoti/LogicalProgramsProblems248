6153061b5b382700b0404dfe6f6d5bfb756c1fd7
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2023-01-30 14:05:44 +0530

    [Jyoti Rao] Check number is Perfect number or not.

---
+++ LogicalPrograms/PerfectNumber.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public class PerfectNumber
+    {
+        public static int number, sum = 0;
+        //2.Check number is Perfect number or not.
+        public static void CheckPerfectNumber()
+        {
+            Console.WriteLine("Enter number for check Perfect Number");
+            number = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    sum = sum + i;
+                }
+            }
+            if (number == sum)
+                Console.WriteLine("{0} is perfect number", number);
+            else
+                Console.WriteLine("{0} is not perfect number", number);
+        }
+    }
+}
--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -2,19 +2,23 @@
 
 namespace LogicalPrograms
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Logical Programs\n------------------------------------");
             Console.WriteLine("Please select any option");
-            Console.WriteLine("1.FindFibonacciSeries\n13.Exit\n");
+            Console.WriteLine("1.FindFibonacciSeries\n2.CheckPerfectNumber\n13.Exit\n");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
                 case 1:
                     Console.Clear();
                     FibonacciSeries.FindFibonacciSeries();
+                    break;
+                case 2:
+                    Console.Clear();
+                    PerfectNumber.CheckPerfectNumber();
                     break;
                 case 13:
                     Console.Clear();
