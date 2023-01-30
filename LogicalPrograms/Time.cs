8fb3d6adafddd7a9e900a8cff267c77f06910540
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2023-01-30 15:43:10 +0530

    [Jyoti Rao] Find the elapsed time between start and end

---
+++ LogicalPrograms/Time.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace LogicalPrograms
+{
+    public class Time
+    {
+        public static int i;
+        public static void FindElapsedTime()
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            stopwatch.Start();
+            Thread.Sleep(5);
+            stopwatch.Stop();
+            Console.WriteLine("Time elapsed: {0}", stopwatch.Elapsed);
+        }
+    }
+}
