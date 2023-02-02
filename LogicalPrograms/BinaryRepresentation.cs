e1f1cd5081731a0f17e5e37ae9f1f8e0eee88e05
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2023-02-02 19:38:02 +0530

    [Jyoti Rao] Find the binary representation of n number

---
+++ LogicalPrograms/BinaryRepresentation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace LogicalPrograms
+{
+    public class BinaryRepresentation
+    {
+        public static int number;
+        //12.To compute the binary representation of n number.
+        public static void DecToBinary()
+        {
+            Console.WriteLine("Please enter number that you want to represent in Binary:");
+            number = Convert.ToInt32(Console.ReadLine());
+            int[] binaryNum = new int[32];
+            int i = 0;
+            while (number > 0)
+            {
+                binaryNum[i] = number % 2;
+                number = number / 2;
+                i++;
+            }
+            for (int j = i - 1; j >= 0; j--)
+            {
+                Console.Write(binaryNum[j]);
+            }
+        }
+    }
+}
--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -8,7 +8,7 @@ namespace LogicalPrograms
         {
             Console.WriteLine("Welcome to the Logical Programs\n------------------------------------");
             Console.WriteLine("Please select any option");
-            Console.WriteLine("1.FindFibonacciSeries\n2.CheckPerfectNumber\n3.CheckPrimeNumber\n4.ReverseNumber\n5.TemperaturConversion\n6.GregorianCalendar\n7.FindMonthlyPayment\n8.FindElapsedTime\n9.SwapNibbles\n10.FindFewestNotes\n11.ComputeSqrtOfNonnegativeNumber\n13.Exit\n");
+            Console.WriteLine("1.FindFibonacciSeries\n2.CheckPerfectNumber\n3.CheckPrimeNumber\n4.ReverseNumber\n5.TemperaturConversion\n6.GregorianCalendar\n7.FindMonthlyPayment\n8.FindElapsedTime\n9.SwapNibbles\n10.FindFewestNotes\n11.ComputeSqrtOfNonnegativeNumber\n12.DecimalToBinayRepresentation\n13.Exit\n");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -56,6 +56,10 @@ namespace LogicalPrograms
                 case 11:
                     Console.Clear();
                    NewtonsMethod.Sqrt();
+                    break;
+                case 12:
+                    Console.Clear();
+                    BinaryRepresentation.DecToBinary();
                     break;
                 case 13:
                     Console.Clear();
